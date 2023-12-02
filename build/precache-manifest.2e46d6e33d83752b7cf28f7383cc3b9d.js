208ba3a9eea23c322c64abdfbc8669e9ac516048
felipeverdugo <felipeverdugo016@gmail.com>
2023-12-02 13:12:38 -0300

    Agregando vista del front

---
+++ build/precache-manifest.2e46d6e33d83752b7cf28f7383cc3b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "458211bbcfa5ccd927ce",
+    "url": "/static/js/runtime~main.31fcaf80.js"
+  },
+  {
+    "revision": "5b614d8d7c813b44db84",
+    "url": "/static/js/main.49562ebb.chunk.js"
+  },
+  {
+    "revision": "eeb29d83d1bcbaba4e91",
+    "url": "/static/js/3.82db40f9.chunk.js"
+  },
+  {
+    "revision": "acfd5e76b9b044f0d368",
+    "url": "/static/js/2.f75f4feb.chunk.js"
+  },
+  {
+    "revision": "5b614d8d7c813b44db84",
+    "url": "/static/css/main.1a46396d.chunk.css"
+  },
+  {
+    "revision": "7b805f584416a239c87d276ba1928486",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.9336f362c9ba4db7aebddec3c4261dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "458211bbcfa5ccd927ce",
+    "url": "/static/js/runtime~main.31fcaf80.js"
+  },
+  {
+    "revision": "1b8f8a4494c102777b63",
+    "url": "/static/js/main.da66b30a.chunk.js"
+  },
+  {
+    "revision": "eeb29d83d1bcbaba4e91",
+    "url": "/static/js/3.82db40f9.chunk.js"
+  },
+  {
+    "revision": "acfd5e76b9b044f0d368",
+    "url": "/static/js/2.f75f4feb.chunk.js"
+  },
+  {
+    "revision": "1b8f8a4494c102777b63",
+    "url": "/static/css/main.1a46396d.chunk.css"
+  },
+  {
+    "revision": "bdc77d050fd3b93eb8311695535494fe",
+    "url": "/index.html"
+  }
+];
